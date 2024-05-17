163e540e9c77e6a25ca18cd1c96399ef690a9f33
Neto Russo <netoacrn1@gmail.com>
2024-05-17 20:48:07 -0300

    subindo o inicio do projeto

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const abrir = document.getElementById("abrirModal");
+const fechar = document.getElementById("fecharModal");
+const fundo = document.getElementById("fundoModal");
+const conteudo = document.getElementById("conteudoModal");
+const modal = document.getElementById("modal");
+let estadoDoModal = false;
+
+function abrirEFechar() {
+    if (estadoDoModal) {
+
+        conteudo.classList.remove("open");
+        conteudo.classList.add("close");
+        setTimeout(()=> {
+            modal.classList.add("clicado");
+            estadoDoModal = false;
+        }, 500)
+
+
+    } else {
+        modal.classList.remove("clicado");
+        conteudo.classList.remove("close");
+        conteudo.classList.add("open");
+        estadoDoModal = true;
+    }
+}
+
+abrir.addEventListener("click", (e) => {
+    e.preventDefault();
+    abrirEFechar()
+});
+
+fechar.addEventListener("click", (e) => {
+    e.preventDefault();
+    abrirEFechar()
+});
+
+fundo.addEventListener("click", () => abrirEFechar());
