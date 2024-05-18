bd8f952064e0e3c0aef169d1bb95c01d564a540d
Neto Russo <netoacrn1@gmail.com>
2024-05-18 04:25:06 -0300

    finalizando o dia com a home quase completa, e cards posicionados

--- js/main.js
+++ js/main.js
@@ -6,32 +6,34 @@ const modal = document.getElementById("modal");
 let estadoDoModal = false;
 
 function abrirEFechar() {
-    if (estadoDoModal) {
+  if (estadoDoModal) {
 
-        conteudo.classList.remove("open");
-        conteudo.classList.add("close");
-        setTimeout(()=> {
-            modal.classList.add("clicado");
-            estadoDoModal = false;
-        }, 500)
+    conteudo.classList.remove("open");
+    conteudo.classList.add("close");
+    setTimeout(() => {
+      modal.classList.add("clicado");
+      estadoDoModal = false;
+    }, 500)
 
 
-    } else {
-        modal.classList.remove("clicado");
-        conteudo.classList.remove("close");
-        conteudo.classList.add("open");
-        estadoDoModal = true;
-    }
+  } else {
+    modal.classList.remove("clicado");
+    conteudo.classList.remove("close");
+    conteudo.classList.add("open");
+    estadoDoModal = true;
+  }
 }
 
 abrir.addEventListener("click", (e) => {
-    e.preventDefault();
-    abrirEFechar()
+  e.preventDefault();
+  abrirEFechar()
 });
 
 fechar.addEventListener("click", (e) => {
-    e.preventDefault();
-    abrirEFechar()
+  e.preventDefault();
+  abrirEFechar()
 });
 
 fundo.addEventListener("click", () => abrirEFechar());
+
+console.log("Olá Amiguinho!");
